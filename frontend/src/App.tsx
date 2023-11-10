3ea5c8590807719d01fec504bb0cd4cab788355a
Luca Favini <luca.favini.02@gmail.com>
2023-11-10 20:59:27 +0100

    frontend: add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,39 @@
-function App() {
+import type { FC } from "react"
+import { useEffect, useState } from "react"
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import Loading from "./components/common/Loading"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: , // each page will get this
+    errorElement: <Navigate to="/dashboard" replace />,
+    children: [
+      { index: true, element: <Navigate to="/dashboard" replace /> },
+      { path: "dashboard", element: <h1>home dashboard</h1> },
+      { path: "add", element: <h1>add</h1> },
+    ]
+  }
+])
+
+const App : FC = () => {
+
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <Loading />
+      </div>
+    )
+  }
+
   return (
-    <>
-      <h1 className='text-rose-500'>Sensors Minds</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ frontend/src/components/common/Loading.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+const Loading : FC = () => {
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
