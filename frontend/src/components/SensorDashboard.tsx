393294917ff0f72b75cbd8ad163453c203f49ada
Luca Favini <luca.favini.02@gmail.com>
2023-11-11 00:43:24 +0100

    frontend: add recharts

--- frontend/src/components/SensorDashboard.tsx
+++ frontend/src/components/SensorDashboard.tsx
@@ -1,12 +1,23 @@
 import { FC } from "react";
 import Sensor from "../interfaces/Sensor";
 
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
 const SensorDashboard : FC<{ sensor: Sensor }> = ({ sensor } : { sensor : Sensor}) => {
 
   return (
     <div>
 
       <h1>Sensor "{sensor.name}"</h1>
+
+      <LineChart width={730} height={250} data={sensor.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="timestamp" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
 
     </div>
   )
