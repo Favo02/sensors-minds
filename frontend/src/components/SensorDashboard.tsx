f76d84ecb037de0a07e303253d7bb0bd6b74e486
Luca Favini <luca.favini.02@gmail.com>
2023-11-11 04:51:54 +0100

    frontend: add sensor selector

--- frontend/src/components/SensorDashboard.tsx
+++ frontend/src/components/SensorDashboard.tsx
@@ -1,18 +1,45 @@
 import { FC } from "react";
 import Sensor from "../interfaces/Sensor";
-
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { IoMdCloseCircle } from "react-icons/io";
+import Select from 'react-select';
 
-const SensorDashboard : FC<{ sensor: Sensor }> = ({ sensor } : { sensor : Sensor}) => {
+interface PropsInt {
+  availableSensors : {label : string, value : string}[]
+  index : number,
+  sensors : undefined | Sensor,
+  removeDashboard : (i : number) => void
+}
+
+const SensorDashboard : FC<PropsInt> = ({ availableSensors, index, sensors, removeDashboard } : PropsInt) => {
+
+  if (!sensors) {
+    return (
+      <div key={index} className="h-[500px] rounded-3xl mx-16 my-16 bg-gradient-to-br from-gray-100 to-gray-300 p-8 shadow-2xl drop-shadow-2xl flex flex-col items-center justify-center relative">
+        <button className="absolute top-6 right-6" onClick={() => removeDashboard(index)}>
+          <IoMdCloseCircle size={30} />
+        </button>
+        <h1 className="text-2xl font-semibold">No sensor selected</h1>
+
+        <div className="w-1/2 mt-4">
+          <Select
+            isMulti
+            name="colors"
+            placeholder="Select one or more sensors"
+            options={availableSensors}
+          />
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="rounded-3xl mx-16 my-16 bg-white p-8 shadow-2xl drop-shadow-2xl">
 
-      <h1 className="px-10 py-6 text-3xl font-semibold italic">Sensor "{sensor.name}"</h1>
+      <h1 className="px-10 py-6 text-3xl font-semibold italic">Sensor "{sensors.name}"</h1>
 
-      {/* <ResponsiveContainer width={"100%"} aspect={1}> */}
       <ResponsiveContainer width={"100%"} height={400}>
-        <LineChart data={sensor.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <LineChart data={sensors.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="timestamp" />
           <YAxis />
@@ -20,7 +47,6 @@ const SensorDashboard : FC<{ sensor: Sensor }> = ({ sensor } : { sensor : Sensor
           <Line type="monotone" dataKey="value" stroke="#3c6fff" strokeWidth={4} />
         </LineChart>
       </ResponsiveContainer>
-
 
     </div>
   )
--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -1,52 +1,36 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import Sensor from "../../interfaces/Sensor";
 import SensorDashboard from "../SensorDashboard";
-import { IoMdCloseCircle } from "react-icons/io"
 
 const Dashboard : FC = () => {
 
   const [dashboards, setDashboards] = useState<(Sensor | undefined)[]>([])
+  const [availableSensors, setAvailableSensors] = useState<{label : string, value : string}[]>([])
 
-  // useEffect(() => {
-  //   fetch("../../../public/mockData.json", {
-  //   // fetch("http://localhost:8080/data/plugmeter1", {
-  //     method: "GET"
-  //   })
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       // setData(data.data.map())
-  //       setData(data)
-  //       console.log(data)
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, [])
+  useEffect(() => {
+    fetch("../../../public/mockSensors.json", {
+    // fetch("http://localhost:8080/sensors", {
+      method: "GET"
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAvailableSensors(data.map((d : string) => ({label: d, value: d})))
+      })
+      .catch((error) => console.log(error))
+
+  }, [])
 
   const addNewDashboard = () => {
     setDashboards(oldArray => [...oldArray, undefined])
   }
 
   const removeDashboard = (toRemove : number) => {
-    setDashboards(dashboards.filter((d, i) => i != toRemove))
+    setDashboards(dashboards.filter((_, i) => i != toRemove))
   }
 
   return (
     <div>
-      {dashboards.map((d, i) => {
-        if (!d) {
-          return (
-            <div key={i} className="h-[500px] rounded-3xl mx-16 my-16 bg-gradient-to-br from-gray-100 to-gray-300 p-8 shadow-2xl drop-shadow-2xl flex items-center justify-center relative">
-              <button className="absolute top-6 right-6" onClick={() => removeDashboard(i)}>
-                <IoMdCloseCircle size={30} />
-              </button>
-              <h1 className="text-2xl font-semibold">No sensor selected</h1>
-            </div>
-          )
-        }
-
-        return (
-          <SensorDashboard key={i} sensor={d} />
-        )
-      })}
+      {dashboards.map((d, i) => <SensorDashboard key={i} availableSensors={availableSensors} index={i} sensors={d} removeDashboard={removeDashboard} /> )}
 
       <div className="rounded-3xl mx-32 mb-12 bg-gradient-to-br from-gray-100 to-gray-300 p-4 shadow-2xl drop-shadow-2xl text-center font-semibold text-xl">
         <button onClick={addNewDashboard}>Add new sensor</button>
